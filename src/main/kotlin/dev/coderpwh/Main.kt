17bc8c0dc5d0b29c84edf376d8fb2723a440725a
pwh-pwh <plmoknpwh@163.com>
2023-09-04 16:31:58 +0800

    接入命令行

--- src/main/kotlin/dev/coderpwh/Main.kt
+++ src/main/kotlin/dev/coderpwh/Main.kt
@@ -1,6 +1,4 @@
-import dev.coderpwh.client.MsgClient
-import dev.coderpwh.client.ClientFactory
-import dev.coderpwh.client.ClientType
+import dev.coderpwh.client.*
 import kotlinx.coroutines.runBlocking
 import java.util.Scanner
 import kotlin.system.exitProcess
@@ -8,26 +6,50 @@ import kotlin.system.exitProcess
 
 suspend fun main(args: Array<String>) {
     val qingyunkeClient = ClientFactory.getClient(ClientType.QingYunKeClient)
+    val imgClient = PollImgClient()
     val sc = Scanner(System.`in`)
     while (true) {
         showMenu()
         val next = sc.next()
         when(next) {
             "1" -> doChat(sc,qingyunkeClient)
-            "2" ->  {
+            "2" -> doImg(sc,imgClient)
+            "3" ->  {
                 println("bye")
                 exitProcess(0)
             }
-
             else -> println("选择不存在，请重新选择")
         }
     }
     val msgResp = qingyunkeClient.sendMsg("你好啊，臭机器人")
     println(msgResp)
 }
-fun doChat(sc:Scanner,client: MsgClient) {
-    showChatMenu()
+
+fun doImg(sc: Scanner, imgClient: ImgClient) {
+    doAction(sc,::showImgMenu) {
+        sc ->
+        val next = sc.next()
+        if(next.startsWith("1:")) {
+            runBlocking {
+                val message = imgClient.getImg(next)
+                println("msg:$message")
+            }
+        } else {
+            return
+        }
+    }
+}
+
+inline fun doAction(sc:Scanner,showMenu:()->Unit, clientAction:(Scanner)->Unit) {
+    showMenu()
     while (true) {
+        clientAction(sc)
+    }
+}
+
+fun doChat(sc:Scanner,client: MsgClient) {
+    doAction(sc,::showChatMenu) {
+            sc ->
         val next = sc.next()
         if(next.startsWith("1:")) {
             runBlocking {
@@ -35,10 +57,9 @@ fun doChat(sc:Scanner,client: MsgClient) {
                 println("msg:$message")
             }
         } else {
-            break
+            return
         }
     }
-
 }
 
 fun showChatMenu() {
@@ -46,7 +67,13 @@ fun showChatMenu() {
     println("other to quit")
 }
 
+fun showImgMenu() {
+    println("1:you prompt;")
+    println("other to quit")
+}
+
 fun showMenu() {
     println("1:chat")
-    println("2:quit")
+    println("2:img")
+    println("3:quit")
 }
