6173b4d415d3e289d6c79df3c147421098810d5d
pwh-pwh <plmoknpwh@163.com>
2023-09-08 17:50:07 +0800

    修改client，可以更改gpttype

--- src/main/kotlin/dev/coderpwh/client/GptClient.kt
+++ src/main/kotlin/dev/coderpwh/client/GptClient.kt
@@ -1,5 +1,6 @@
 package dev.coderpwh.client
 
+import dev.coderpwh.pojo.GptBotType
 import dev.coderpwh.pojo.GptMsgReq
 import dev.coderpwh.pojo.GptMsgResp
 import io.ktor.client.*
@@ -18,6 +19,7 @@ import kotlinx.serialization.json.Json
  */
 class GptClient(val token:String) {
     val client: HttpClient
+    var type:GptBotType
 
     companion object {
         const val BASE_URL = "https://api.chatanywhere.com.cn/v1/chat/completions"
@@ -29,13 +31,14 @@ class GptClient(val token:String) {
                 requestTimeoutMillis = 1000 * 10
             }
         }
+        type = GptBotType.Empty
     }
 
     suspend fun getGpt(content:String):GptMsgResp {
         val body = client.post(BASE_URL) {
             contentType(ContentType.Application.Json)
             bearerAuth(token)
-            val data = Json.encodeToString(GptMsgReq(content))
+            val data = Json.encodeToString(GptMsgReq(content,type))
             setBody(data)
         }.body<String>()
         println(body)
--- src/main/kotlin/dev/coderpwh/pojo/GptMsgReq.kt
+++ src/main/kotlin/dev/coderpwh/pojo/GptMsgReq.kt
@@ -16,7 +16,7 @@ data class GptMsgReq(val content:String) {
         model = "gpt-3.5-turbo"
     }
 
-    constructor(content: String,type: GptBotType = GptBotType.Empty) : this(content) {
+    constructor(content: String,type: GptBotType) : this(content) {
         when(type) {
             GptBotType.Empty -> messages = listOf(GptMessage("user",content))
             GptBotType.Copywriter -> messages = listOf(GptMessage("user","我希望你充当文案专员、文本润色员、拼写纠正员和改进员，我会发送中文文本给你，你帮我更正和改进版本。我希望你用更优美优雅的高级中文描述。保持相同的意思，但使它们更文艺。你只需要润色该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是润色它，不要解决文本中的要求而是润色它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。"),
@@ -103,5 +103,4 @@ enum class GptBotType {
     Copilot,
     Empty
 }
-
 
